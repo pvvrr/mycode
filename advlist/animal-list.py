543d508898b9ab5e224abfb0d2f381a10970c001
pvvrr <pvvramarao@gmail.com>
2023-01-24 15:46:48 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def main():
+
+    my_animala_list = ["cat", "dog", "horse","mouse", "bat"]
+
+    print(my_animala_list[0], my_animala_list[1], my_animala_list[-1])
+
+
+
+
+
+
+
+
+main()
