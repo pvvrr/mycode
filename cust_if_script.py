e4eb81da2e57f0f17c4781ff3d51185e38ca4070
pvvrr <pvvramarao@gmail.com>
2023-01-25 18:41:05 +0000

    Refactoed code

--- cust_if_script.py
+++ cust_if_script.py
@@ -8,17 +8,17 @@
 
 attainedscore = int(input("Please enter the score got in the exam : "))
 
-if attainedscore <= 59:
+if attainedscore <= 59: # Grading (59 and below) F
     print("Your grade is : ", "F")
-elif attainedscore <= 69 and attainedscore > 59:
+elif attainedscore <= 69 and attainedscore > 59: # Grading (69 to 60) D
     print("Your grade is ", "D")
-elif attainedscore <= 79 and attainedscore > 69:
+elif attainedscore <= 79 and attainedscore > 69: # Grading (79 to 70) C
     print("Your grade is ", "C")
-elif attainedscore <= 89 and attainedscore > 79:
+elif attainedscore <= 89 and attainedscore > 79: # Grading (89 to 80) B
     print("Your grade is ", "B")
-elif attainedscore <= 100 and attainedscore > 89:
+elif attainedscore <= 100 and attainedscore > 89: # Grading (100 to 90) A
     print("Your grade is ", "A")
-else:
+else: # unabel to grade hence displaying Warning
     print(" WARNING !!! Unable to grade the Score")
 print ("********************Reached the end")
 
