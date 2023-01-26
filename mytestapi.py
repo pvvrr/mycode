49ec91ae4824f56c1a009fa9004bde72cd6478f6
pvvrr <pvvramarao@gmail.com>
2023-01-26 20:18:09 +0000

    another test program for API test

---
+++ mytestapi.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import requests
+
+AOIF = "https://www.anapioficeandfire.com/api/characters/"
+
+def main():
+    ## Send HTTPS GET to the API of ICE and Fire
+    gotresp = requests.get(AOIF)
+
+    ## Decode the response
+    got_dj = gotresp.json()
+
+    ## print the response
+    print(got_dj)
+    
+    ## display only the keys within
+    ## the dictionary by using dict.keys()
+    ## great for seeing what keys are available for lookup
+    print(got_dj.keys())
+    
+
+if __name__ == "__main__":
+    main()
+
